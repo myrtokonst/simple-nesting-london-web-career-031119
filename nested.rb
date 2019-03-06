4a01046c3385a30b17f91a9684b47301bfbb03bb
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-06 10:51:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,9 +84,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {
-    known_for: "Ruby",
-    languages:["LISP", "C"]}
+programmer_hash[:yukihiro_matsumoto]= {:known_for => "Ruby",
+:languages => ["LISP", "C"]}
 end
 adding_matz 
 
