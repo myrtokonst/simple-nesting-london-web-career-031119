0a4680a1e56fb947afbe51198b2333fba59492b4
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-06 10:40:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,9 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
-dennis_ritchies_language.join
+dennis_ritchies_language
 
 def adding_matz
 # add the following information to the top level of programmer_hash
