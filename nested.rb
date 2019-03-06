4aacf4edb3343ede626a204d8cee1424aca91a0a
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-06 10:37:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,9 +16,9 @@ def hopper
         }
      }
 
-
+ programmer_hash[:grace_hopper]
 end
-
+hopper
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
@@ -101,8 +101,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+    
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
